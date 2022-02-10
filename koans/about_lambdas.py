c34eee75724ac8cd048ea6adf82a3de2db8934af
ShaunR23 <shaunradford23@gmail.com>
2022-02-09 22:35:39 -0500

    finished lamda

--- koans/about_lambdas.py
+++ koans/about_lambdas.py
@@ -10,9 +10,13 @@ from runner.koan import *
 class AboutLambdas(Koan):
     def test_lambdas_can_be_assigned_to_variables_and_called_explicitly(self):
         add_one = lambda n: n + 1
-        self.assertEqual(__, add_one(10))
+        self.assertEqual(11, add_one(10))
 
     # ------------------------------------------------------------------
+
+    #A lambda function can take any number of arguments, but can only have one expression.
+    #single-line function declared with no name
+
 
     def make_order(self, order):
         return lambda qty: str(qty) + " " + order + "s"
@@ -21,8 +25,8 @@ class AboutLambdas(Koan):
         sausages = self.make_order('sausage')
         eggs = self.make_order('egg')
 
-        self.assertEqual(__, sausages(3))
-        self.assertEqual(__, eggs(2))
+        self.assertEqual('3 sausages', sausages(3))
+        self.assertEqual('2 eggs', eggs(2))
 
     def test_accessing_lambda_without_assignment(self):
-        self.assertEqual(__, self.make_order('spam')(39823))
+        self.assertEqual('39823 spams', self.make_order('spam')(39823))
